fa84a6c280f422b00631f189e91b0683ba2d730c
Alex Dowsett <adowsett@diligent.com>
2019-10-24 16:47:08 +0100

    second file

---
+++ namebuilder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.celestial.gitflow.walkthrough;
+
+/**
+ *
+ * @author Selvyn
+ */
+class NameBuilder
+{
+
+    public  String buildName(String[] input)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
